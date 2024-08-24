6929c7a9141b23cdb5a065ad9ac49adae2a23c68
StefanoRimoldi <148816497+StefanoRimoldi@users.noreply.github.com>
2024-08-24 20:10:17 +0200

    Update NewsManagerLib.sol
    aggiornamento Libreria

--- NewsManagerLib.sol
+++ NewsManagerLib.sol
@@ -1,19 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
+import "./NewsManager.sol"; 
+
 library NewsManagerLib {
     
-    function isNewsValidatedByMinValidators(uint256 minValidations, uint256 validationCount) internal pure returns (bool) {
-        return validationCount >= minValidations;
+    function isNewsValidatedByMinValidators(
+        NewsManager.News memory news
+    ) internal pure returns (bool) {
+        return news.validationCount >= news.minValidations;
     }
 
     
-    function isValidatorRegistered(mapping(address => bool) storage validators, address _validator) internal view returns (bool) {
+    function isValidatorRegistered(
+        mapping(address => bool) storage validators,
+        address _validator
+    ) internal view returns (bool) {
         return validators[_validator];
     }
 
     
     function getValidatorCount(address[] storage validatorList) internal view returns (uint256) {
         return validatorList.length;
+    }
+
+    // Controlla se un validatore ha già validato la notizia
+    function hasValidatorValidated(mapping(address => bool) storage validators, address _validator) internal view returns (bool) {
+        return validators[_validator];
     }
 }
